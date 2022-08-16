60b4345675ea5b971a354eac19d4fb4fb2a688a4
DiegoArredondoFirstlight <diego.arredondo@wotdev.com>
2022-08-16 00:25:25 -0600

    messages senders implemented

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import time
 from getpass import getpass
 import asyncio
-from xmpp import MainClient, RegisterClient
+import threading
+from xmpp import Client, RegisterClient, DeleteClient
 
 statusArray = ["AvailAble", "Unavailable", "IN A Meeting", "AFK (Away From Keyboard)",]
 
@@ -38,11 +39,9 @@ def start():
             jid = input("JID (example@alumchat.fun): ")
             password = getpass("Password: ")
             status = "Available"
-
             while True:
                 statusOpt = 0
                 statusMsg = "Available"
-                
                 try:
                     print("1. Available \n2. Unavailable \n3. AFK \n4. In a meeting")
                     statusOpt = int(input('Choose your status: '))
@@ -59,8 +58,170 @@ def start():
                 status = statusArray[statusOpt - 1]
                 break
             
-            xmpp_client = MainClient(jid, password, status, statusMsg)
+            xmpp_client = Client(jid, password, status, statusMsg)
             xmpp_client.connect()
+
+            # Threading options
+            stop_threads = False
+            xmpp_app_thread = threading.Thread(target = app_thread, args=(xmpp_client, lambda : stop_threads,))
+            xmpp_app_thread.start()
+
+            time.sleep(3)
+
+            # Timeout connection
+            if xmpp_client.is_client_offline:
+                time.sleep(2)
+                if xmpp_client.is_client_offline:
+                    print("Can't connect to OpenFire server, try again.\n")
+                    stop_threads = True
+                    xmpp_app_thread.join()
+                    return
+
+            # When Logged
+            while True:
+                try:
+                    print("1. Private Messages \n2. Send Private Message \n3. Add contact \n4. Show contacts \n5. Room Chats \n6. Logout")
+                    secondary_option = int(input('Choose one: '))
+                    # Check option is in range
+                    if secondary_option < 1 or secondary_option > 7:
+                        print("Choose a valid option")
+                        continue
+                except:
+                    print("Choose a valid option")
+                    continue
+                
+                # Show private messages
+                if secondary_option == 1:
+                    if len(xmpp_client.messages.keys()) == 0:
+                        print("Your don't have new messages.")
+                        continue
+                    print("Choose one of the following chats:")
+                    chatNumber = 1
+                    for key in xmpp_client.messages.keys():
+                        print(f"{chatNumber}. Chat with {key}")
+                        chatNumber += 1
+
+                    # User select conversation
+                    try:
+                        chat = int(input("Choose one: "))
+                    except:
+                        print("Choose a valid option")
+                        continue
+                    
+                    if chat < 1 or chat > len(xmpp_client.messages.keys()):
+                        print("Choose a valid option")
+                    else:
+                        # Get user and messages
+                        otherUser = list(xmpp_client.messages.keys())[chat - 1]
+                        messages_sent = xmpp_client.messages[otherUser]["messages"]
+                        xmpp_client.last_chat_with = otherUser
+                        xmpp_client.pm_send_state_message(f"{otherUser}@alumchat.fun", "active")
+
+                        print(f"\n--------- Chat with {otherUser} ---------")
+                        print("* Write and press enter to respond (-f for Send a File and /exit or /e to Exit) *")
+
+                        for message_history in messages_sent:
+                            print(message_history)
+
+                        while True:
+                            message = input('-> ')
+
+                            # Excape reserved word
+                            if message == '/exit' or message == '/e' :
+                                xmpp_client.pm_send_state_message(f"{otherUser}@alumchat.fun", "paused")
+                                break
+                            elif '-f ' in message:
+                                #file sender
+                                pass
+                            else:
+                                # Send Response  
+                                xmpp_client.direct_message(f"{otherUser}@alumchat.fun", message)
+
+                        xmpp_client.current_chat_with = None
+
+                # Send private message
+                elif secondary_option == 2:
+                    message_to = input("To: ")
+                    message = input("Message: ")
+                    xmpp_client.direct_message(message_to, message)
+                
+                # Add contact
+                elif secondary_option == 3:
+                    message_to = input("Name: ")
+                    xmpp_client.send_contact_subscription(message_to)
+                
+                # Show contacts info
+                elif secondary_option == 4:
+                    print("\n ----------- CONTACTS ----------- ")
+                    xmpp_client.show_contacts()
+
+                # Room options
+                elif secondary_option == 5:
+                    while True:
+                        try:
+                            print("1. Join room \n2. Create room \n3. Show rooms \n4. Go Back")
+                            room_option = int(input('> '))
+
+                            # Check option is in range
+                            if room_option < 1 or room_option > 4:
+                                print("Choose a valid option")
+                                continue
+
+                            break
+                        except:
+                            room_option = 4
+                            break
+                    
+                    # Go Back (default option)
+                    if room_option == 4:
+                        continue
+                    
+                    # Show rooms
+                    elif room_option == 3:
+                        xmpp_client.muc_discover_rooms()
+                        continue
+                    
+                    # Room Options
+                    room = input("Room: ")
+                    username = input("Username: ")
+
+                    # Create Room
+                    if room_option == 2:
+                        asyncio.run(xmpp_client.muc_create_room(room, username))
+                    
+                    # Join room
+                    elif room_option == 1:
+                        xmpp_client.muc_join(room, username)
+
+                    print(f"\n--------- Group Chat @ {room} ---------")
+                    print("* write and press enter to respond (-exit or /e to Exit) *")
+
+                    while True:
+                        try:
+                            message = input('--> ')
+                            if message == '/exit' or message == '/e':
+                                break
+
+                            xmpp_client.muc_send_message(message)
+                        except:
+                            continue
+                    
+                    # Leave room on exit
+                    xmpp_client.muc_exit_room()
+
+                # Logout
+                elif secondary_option == 6:
+                    stop_threads = True
+                    xmpp_app_thread.join()
+                    break
+
+        # Remove account
+        elif opt == 3:
+            jid = input("JID: ")
+            password = getpass("Password: ")
+            xmpp_client = DeleteClient(jid, password)
+            xmpp_client.connect()
+            xmpp_client.process(forever=False)
 
         # Exit
         elif opt == 4:
--- xmpp.py
+++ xmpp.py
@@ -1,4 +1,9 @@
 import slixmpp
+import asyncio
+import webbrowser
+import validators
+import mimetypes
+import uuid
 from slixmpp.exceptions import IqError, IqTimeout
 import xml.etree.ElementTree as ET
 
@@ -19,17 +24,37 @@ class MainClient(slixmpp.ClientXMPP):
         self.register_plugin('xep_0363') # Files
         self.register_plugin('xep_0065') # SOCKS5 Bytestreams
         self.register_plugin('xep_0085') # Notifications
-        
-        self.local_jid = jid
-        self.nickname = jid[:jid.index("@")]
-        self.status = status
-        self.status_message = status_message
 
         # Event handlers
         self.add_event_handler("session_start", self.start)
+        self.add_event_handler("message", self.message)
+        self.add_event_handler("presence_subscribe", self.new_subscription)
+        self.add_event_handler("got_online", self.got_online)
+        self.add_event_handler("got_offline", self.got_offline)
+        self.add_event_handler("groupchat_message", self.muc_message)
+        self.add_event_handler("disco_info",self.show_info)
+        self.add_event_handler("disco_items", self.show_info)
+        self.add_event_handler("chatstate", self.show_chatstate)
         
+        self.local_jid = jid
+        self.alias = jid[:jid.index("@")]
+        self.status = status
+        self.status_message = status_message
 
-    async def start(self, event):
+        # App logic
+        self.messages = {}
+        self.contacts = {}
+        self.actualRoom = ""
+        self.roomOwner = False
+        self.lastChat = None
+        self.clientOffline = True
+
+        # Auto authorize & subscribe on subscription received
+        self.roster.auto_authorize = True
+        self.roster.auto_subscribe = True
+        self.outter_presences = asyncio.Event()
+
+    async def start(self):
         # Send presence
         self.send_presence(pshow=self.status, pstatus=self.status_message)
         
@@ -37,10 +62,296 @@ class MainClient(slixmpp.ClientXMPP):
             # Ask for everyone in server
             await self.get_roster()
             print("\n Login successfully: ",self.local_jid)
-            self.is_client_offline = False
+            self.clientOffline = False
         except:
             print("Error on login! Try Again")
             self.disconnect()
+
+    # Add contact
+    def send_contact_subscription(self, recipient):
+        try:
+            # Subscribe
+            self.send_presence_subscription(recipient, self.local_jid)
+            print("Contact added: ", recipient)
+        except:
+            print("Error adding contact!")
+    
+    # Messages
+    def message(self, message):
+        if message['type'] == "chat":
+
+            sender = str(message['from'])
+            sender = sender[:sender.index("@")]
+            body = str(message['body'])
+            
+            currentMsg = f"{sender}: {body}"
+
+            if sender in self.messages.keys():
+                self.messages[sender]["messages"].append(currentMsg)
+            else:
+                self.messages[sender] = {"messages": [currentMsg]}
+
+            if not self.lastChat == sender:
+                print(f"\n NEW MESSAGE FROM {sender}")
+            else:
+                # If is a message from last chat, just print it
+                print(f"\n{currentMsg}")
+                print("--> ")
+                # Receive images and docs
+                if sender != self.alias and validators.url(body):
+                    webbrowser.open(body)
+
+    def direct_message(self, recipient, message = ""):
+        self.send_message(
+            mto = recipient, 
+            mbody = message, 
+            mtype = "chat", 
+            mfrom = self.local_jid
+        )
+
+        recipient = recipient[:recipient.index("@")]
+        sender = self.local_jid[:self.local_jid.index("@")]
+
+        # Final message
+        currentMsg = f"{sender}: {message}"
+
+        if recipient in self.messages.keys():
+            self.messages[recipient]["messages"].append(currentMsg)
+        else:
+            self.messages[recipient] = {"messages":[currentMsg]}
+
+    def muc_message(self, message = ""):
+        alias = str(message['mucnick'])
+        body = str(message['body'])
+
+        prevSender = alias == self.alias
+        valid = self.actualRoom in str(message['from'])
+        currentMsg = f"{alias}: {body}"
+
+        if not prevSender and valid:
+            print(currentMsg)
+            print("--> ")
+
+    def muc_send_message(self, message):
+        self.send_message(mto=self.actualRoom, mbody=message, mtype="groupchat")
+
+    def pm_send_state_message(self, recipient, state):
+        sender = self.boundjid.bare
+
+        message = self.make_message(
+            mto=recipient,
+            mfrom=self.boundjid.bare,
+            mtype="chat"
+        )
+        message["chat_state"] = state
+        message.send()
+    
+    # Rooms
+    async def muc_create_room(self, room, alias):
+        self.actualRoom = room
+        self.alias = alias
+        self.roomOwner = True
+        
+        # Create
+        self['xep_0045'].join_muc(room, alias)
+        self['xep_0045'].set_affiliation(room, self.boundjid, affiliation='owner')
+
+        # Event handlers
+        self.add_event_handler(f"muc::{self.actualRoom}::got_online", self.muc_on_join)
+        self.add_event_handler(f"muc::{self.actualRoom}::got_offline", self.muc_on_left)
+
+        try:
+            query = ET.Element('{http://jabber.org/protocol/muc#owner}query')
+            elem = ET.Element('{jabber:x:data}x', type='submit')
+            query.append(elem)
+
+            iq = self.make_iq_set(query)
+            iq['to'] = room
+            iq['from'] = self.boundjid
+            iq.send()
+        except:
+            print("Error on create room")
+
+    def muc_discover_rooms(self):
+        try:
+            asyncio.run(self.print_my_rooms())
+        except (IqError, IqTimeout):
+            print("Error on discovering rooms")
+
+    async def print_my_rooms(self):
+        try:
+            rooms = await self['xep_0030'].get_items(jid = f"conference.alumchat.fun", iterator=True)
+        except (IqError, IqTimeout):
+            print("Error on discovering rooms")
+
+
+    def muc_exit_room(self, message = ''):
+        self['xep_0045'].leave_muc(self.actualRoom, self.alias, msg=message)
+        # Reset
+        self.roomOwner = False
+        self.actualRoom = None
+        self.alias = ''
+
+    def muc_join(self, room, alias):
+        # Set local atributes for room
+        self.actualRoom = room
+        self.alias = alias
+
+        # Join
+        self['xep_0045'].join_muc(room, alias, wait=True, maxhistory=False)
+        self.add_event_handler(f"muc::{self.actualRoom}::got_online", self.muc_on_join)
+        self.add_event_handler(f"muc::{self.actualRoom}::got_offline", self.muc_on_left)
+
+
+    def muc_on_join(self, presence):
+        if presence['muc']['nick'] == self.alias:
+            print("Joined to your own room!")
+            # Affiliation if its owner
+        
+        else:
+            alias = str(presence['muc']['nick'])
+            if self.roomOwner:
+                self['xep_0045'].set_affiliation(self.actualRoom, nick=alias, affiliation="member")
+            print(f"{alias} has arrived to the room!")
+        
+
+    def muc_on_left(self, presence):
+        if presence['muc']['nick'] != self.alias:
+            alias = presence['muc']['nick']
+            print(f"{alias} left the room!")
+
+    def got_online(self, event):
+        sender = str(event['from'])
+        if "conference" in sender:
+            return
+        sender = sender[:sender.index("@")]
+        eventShow = ""
+        eventSts = ""
+
+        # Try getting the show and status
+        try:
+            eventShow = str(event['show'])
+            eventSts = str(event['status'])
+        except:
+            eventShow = "Available"
+            eventSts = "Available"
+
+        self.contacts[sender] = {
+            "from": sender, 
+            "show": eventShow, 
+            "status": eventSts
+        }
+
+        if not sender == self.local_jid[:self.local_jid.index("@")]:
+            print(f"{sender} IS ONLINE NOW. ({eventSts})")
+
+    def got_offline(self, event):
+        sender = str(event['from'])
+        if "conference" in sender:
+            return
+
+        sender = sender[:sender.index("@")]
+        self.contacts[sender]["show"] = "Unavailable"
+        self.contacts[sender]["status"] = "Unavailable"
+
+        print(f"{sender} IS NOW OFFLINE")
+
+    def got_disconnected(self):
+        createdPresence = self.Presence()
+        createdPresence['type'] = "Unavailable"
+        # Send stanza
+        createdPresence.send()
+
+    # Show info
+    def show_info(self, iq):
+        if str(iq['type']) == 'result' \
+            and "conference" in str(iq['from']):
+            
+            order = 1
+            textShow = ""
+            
+            print("\n Rooms: ")
+            for char in str(iq):
+                if len(textShow) > 7 and char == '/':
+                    print(f"{str(order)}. {textShow}")
+                    textShow = ""
+                    order += 1
+                    continue
+
+                elif "jid=" in textShow:
+                    textShow += char
+                    continue
+                
+                if char in ('j', 'i', 'd', '='):
+                    textShow += char
+                    if char == 'j':
+                        textShow = char
+                else:
+                    # Reset
+                    textShow = ''
+        return
+
+    def show_chatstate(self, message):
+        sender = str(message['from'])
+        state = message["chat_state"]
+
+        username = sender[:sender.index("@")]
+        if self.lastChat == username:
+            print(f"\n{username} status changed to {state}")
+            print("--> ")
+
+    # Contacts
+    def show_contacts(self):
+        self.get_roster()
+        contacts = self.roster[self.local_jid]
+
+        if(len(contacts.keys()) == 0):
+            print("No contacts yet...")
+
+        for contact in contacts.keys():
+            if contact != self.local_jid:
+                # Print contact info
+                print(f"Contact: {contact}")
+                alias = contact
+
+                if alias in self.contacts.keys():
+                    info = self.contacts[alias]['show']
+                    status = self.contacts[alias]['status']
+                    print(f" INFO: { info }")
+                    print(f" STATUS: { status }")
+
+                else:
+                    print(f" INFO: Unavailable")
+                    print(f" STATUS: Unavailable")
+
+                # Print general info
+                print(f" - GROUPS: { contacts[alias]['groups'] }")
+                print(f" - SUBS: { contacts[alias]['subscription'] }")
+            
+    # Presence and status
+    def new_subscription(self, createdPresence):
+        roster = self.roster[createdPresence['to']]
+        item = self.roster[createdPresence['to']][createdPresence['from']]
+        autocreate = False
+
+        if item["whitelisted"]:
+            item.authorize()
+            if roster.auto_subscribe:
+                autocreate = True
+
+        # Auto authorize
+        elif roster.auto_authorize:
+            item.authorize()
+            if roster.auto_subscribe:
+                autocreate = True
+
+        elif roster.auto_authorize == False:
+            item.unauthorize()
+
+        # Subscribe
+        if autocreate:
+            item.subscribe()
+
 
 class RegisterClient(slixmpp.ClientXMPP):
 
@@ -58,13 +369,13 @@ class RegisterClient(slixmpp.ClientXMPP):
         self.add_event_handler("session_start", self.start)
         self.add_event_handler("register", self.register)
 
-    async def start(self, event):
+    async def start(self):
         self.send_presence()
         # Auto register & disconnect
         await self.get_roster()
         self.disconnect()
 
-    async def register(self, iq):
+    async def register(self):
         res = self.Iq()
         res['type'] = 'set'
         res['register']['username'] = self.boundjid.user
@@ -75,6 +386,45 @@ class RegisterClient(slixmpp.ClientXMPP):
             print(f"Account registered successfully: {self.boundjid}!")
         except IqError as e:
             print(f"Error on register new account: {e.iq['error']['text']}")
+            self.disconnect()
+        except IqTimeout:
+            print("No response from server.")
+            self.disconnect()
+
+
+class DeleteClient(slixmpp.ClientXMPP):
+
+    def __init__(self, jid, password):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+
+        # Plugins
+        self.register_plugin('xep_0030') # Service Discovery
+        self.register_plugin('xep_0004') # Data forms
+        self.register_plugin('xep_0066') # Out-of-band Data
+        self.register_plugin('xep_0077') # In-band Registration
+
+        # Event handler
+        self.add_event_handler("session_start", self.start)
+
+    async def start(self):
+        self.send_presence()
+        await self.get_roster()
+        # Delete & disconnect
+        await self.delete()
+        self.disconnect()
+
+    async def delete(self):
+        response = self.Iq()
+        response['type'] = 'set'
+        response['from'] = self.boundjid.user
+        response['password'] = self.password
+        response['register']['remove'] = 'remove'
+
+        try:
+            await response.send()
+            print(f"Account delete successfully: {self.boundjid}!")
+        except IqError as e:
+            print(f"Couldn't delete account: {e.iq['error']['text']}")
             self.disconnect()
         except IqTimeout:
             print("No response from server.")
